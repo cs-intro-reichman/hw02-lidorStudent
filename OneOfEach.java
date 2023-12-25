eef1b3eb729fd9e3cc9c851c924bc835d5895394
Lidor Amraby <lidoryosef.amraby@post.runi.ac.il>
2023-12-25 23:12:06 +0200

    one of each works fine

--- OneOfEach.java
+++ OneOfEach.java
@@ -1,10 +1,26 @@
-
 /**
  *  Simulates the formation of a family in which the parents decide 
  *  to have children until they have at least one child of each gender.
  */
 public class OneOfEach {
 	public static void main (String[] args) {
-		//// Put your code here
+		int numOfChildren = 0;
+		double randomGender = 0;
+		boolean haveBoy = false;
+		boolean haveGirl = false;
+		while (!(haveBoy && haveGirl)) {
+			randomGender = Math.random();
+			if (randomGender < 0.5) {
+				haveBoy = true;
+				++numOfChildren;
+				System.out.print("b ");
+			} else {
+				haveGirl = true;
+				++numOfChildren;
+				System.out.print("g ");
+			}
+		}
+		System.out.println();
+		System.out.println("You made it... and you now have " + numOfChildren + " children.");
 	}
 }
