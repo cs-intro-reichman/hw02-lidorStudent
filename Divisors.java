c536082a3f9d8ada14e00d55537d63a953b3bd90
Lidor Amraby <lidoryosef.amraby@post.runi.ac.il>
2023-12-25 19:12:43 +0200

    built for loop

--- Divisors.java
+++ Divisors.java
@@ -3,6 +3,12 @@
  */
 public class Divisors {
 	public static void main (String[] args) {
-		
+		int num = Integer.parseInt(args[0]);
+		int divisor = 1;
+		int remainder = 0;
+		for (int i = 0; i <= num; i++) {
+			
+
+		}
 	}
 }
