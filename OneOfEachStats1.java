12671b38c62e67a4ec1989daa8dd1378f24572bc
Lidor Amraby <lidoryosef.amraby@post.runi.ac.il>
2023-12-26 00:06:30 +0200

    one of each stats 1, works

--- OneOfEachStats1.java
+++ OneOfEachStats1.java
@@ -6,6 +6,49 @@
  */
 public class OneOfEachStats1 {
 	public static void main (String[] args) {
-		//// Put your code here
+		int numOfExperiments = Integer.parseInt(args[0]);
+		double countAllChildren = 0;
+		int twoChildren = 0, threeChildren = 0, fourOrMoreChildren = 0;
+		for (int i = 0; i < numOfExperiments; i++) {
+			int numOfChildren = 0;
+			double randomGender = 0;
+			boolean haveBoy = false;
+			boolean haveGirl = false;
+			while (!(haveBoy && haveGirl)) {
+				randomGender = Math.random();
+				if (randomGender < 0.5) {
+					haveBoy = true;
+					++numOfChildren;
+					//System.out.print("b ");
+				} else {
+					haveGirl = true;
+					++numOfChildren;
+					//System.out.print("g ");
+				}
+			}
+			if (numOfChildren == 2) {
+				++twoChildren;
+			} else if (numOfChildren == 3) {
+				++threeChildren;
+			} else {
+				++fourOrMoreChildren;
+			}
+			countAllChildren += numOfChildren;
+			//System.out.println();
+			//System.out.println("You made it... and you now have " + numOfChildren + " children.");
+		}
+		System.out.println("Average: " + (countAllChildren / numOfExperiments) +
+						   " children to get at least one of each gender.");
+		System.out.println("Number of families with 2 children: " + twoChildren);
+		System.out.println("Number of families with 3 children: " + threeChildren);
+		System.out.println("Number of families with 4 or more children: " + fourOrMoreChildren);
+		System.out.print("The most common number of children is ");
+		if (Math.max(Math.max(twoChildren, threeChildren), fourOrMoreChildren) == twoChildren) {
+			System.out.println("2.");
+		} else if (Math.max(Math.max(twoChildren, threeChildren), fourOrMoreChildren) == threeChildren) {
+			System.out.println("3.");
+		} else {
+			System.out.println("4 or more.");
+		}
 	}
 }
