a1a76cb9b1c70b71db8ddffa0565dfcd5e6bc207
Lidor Amraby <lidoryosef.amraby@post.runi.ac.il>
2023-12-25 22:06:13 +0200

    perfect works good

--- Perfect.java
+++ Perfect.java
@@ -3,6 +3,20 @@
  */
 public class Perfect {
 	public static void main (String[] args) {
-		//// Put your code here
+		int num = Integer.parseInt(args[0]);
+		int sum = 1;
+		String yesPerfectNumMessage = num + " is a perfect number since " + num + " = 1";
+		String notPerfectNumMessage = num + " is not a perfect number";
+		for (int i = 2; i < num; i++) {
+			if ((num % i) == 0) {
+				sum += i;
+				yesPerfectNumMessage += " + " + i;
+			}
+		}
+		if (num == sum) {
+			System.out.println(yesPerfectNumMessage);
+		} else {
+			System.out.println(notPerfectNumMessage);
+		}
 	}
 }
