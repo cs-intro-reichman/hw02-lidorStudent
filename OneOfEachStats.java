6c4c67e1ba9e06010355ed4883df5b3c8f94effe
Lidor Amraby <lidoryosef.amraby@post.runi.ac.il>
2023-12-26 00:25:39 +0200

    one of each stats works fine

--- OneOfEachStats.java
+++ OneOfEachStats.java
@@ -9,22 +9,9 @@ import java.util.Random;
  */
 public class OneOfEachStats {
 	public static void main (String[] args) {
-		// Gets the two command-line arguments
 		int numOfExperiments = Integer.parseInt(args[0]);
 		int seed = Integer.parseInt(args[1]);
-		// Initailizes a random numbers generator with the given seed value
         Random generator = new Random(seed);  
-		
-		//// In the previous version of this program, you used a statement like:
-		//// double rnd = Math.random();
-		//// Where "rnd" is the variable that stores the generated random value.
-		//// In this version of the program, replace this statement with:
-		//// double rnd = generator.nextDouble();
-		//// This statement will generate a random value in the range [0,1),
-		//// just like you had in the previous version, except that the 
-		//// randomization will be based on the given seed.
-		//// This is the only change that you have to do in the program.
-
 		int twoChildren = 0;
 		int threeChildren = 0;
 		int fourOrMoreChildren = 0;
