81714cea1b7ea3dd33e25dc5387a74f19da78b86
Lidor Amraby <lidoryosef.amraby@post.runi.ac.il>
2023-12-25 22:46:25 +0200

    damka works, but trying in a different way cause tests dont pass

--- DamkaBoard.java
+++ DamkaBoard.java
@@ -5,7 +5,7 @@ public class DamkaBoard {
 	public static void main(String[] args) {
 		int size = Integer.parseInt(args[0]);
 		for (int i = 0; i < size; i++) {
-			for (int j = 0; j < 2*size; j++) {
+			for (int j = 0; j < (2 * size); j++) {
 				if ((i + j) % 2 == 0) {
 					System.out.print("*");
 				} else {
