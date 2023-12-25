7680df30cc88af6be9f0483d1734a8ba8a305656
Lidor Amraby <lidoryosef.amraby@post.runi.ac.il>
2023-12-26 00:22:36 +0200

    one of each stats works fine

--- OneOfEachStats.java
+++ OneOfEachStats.java
@@ -10,7 +10,7 @@ import java.util.Random;
 public class OneOfEachStats {
 	public static void main (String[] args) {
 		// Gets the two command-line arguments
-		int T = Integer.parseInt(args[0]);
+		int numOfExperiments = Integer.parseInt(args[0]);
 		int seed = Integer.parseInt(args[1]);
 		// Initailizes a random numbers generator with the given seed value
         Random generator = new Random(seed);  
@@ -25,7 +25,6 @@ public class OneOfEachStats {
 		//// randomization will be based on the given seed.
 		//// This is the only change that you have to do in the program.
 
-		int numOfExperiments = Integer.parseInt(args[0]);
 		int twoChildren = 0;
 		int threeChildren = 0;
 		int fourOrMoreChildren = 0;
